552fbc4deed4ac3262d46a730ab3a81044229b53
pranav <pranavkalal36@gmail.com>
2025-03-30 19:13:53 +1000

    Fix axios baseURL for production

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,11 +1,10 @@
+
+
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api', // NGINX will forward /api to http://localhost:5002
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://54.206.62.117:5002/api',
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
-
