7a290c68ddbbe1d73386ead71ca8d2aa8cb6a7a2
pranav <pranavkalal36@gmail.com>
2025-03-30 19:00:07 +1000

    fix: updated axios baseURL for NGINX proxy

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://54.206.62.117:5002', // local
-  //baseURL: 'http://54.206.62.117:5002', // live
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: '/api', // NGINX will forward /api to http://localhost:5002
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default axiosInstance;
+
